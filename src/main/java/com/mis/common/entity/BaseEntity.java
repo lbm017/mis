4eae60e3b0df8e948d566967dbe7b181b469f436
lbm017 <lbm017@126.com>
2018-04-03 18:04:43 +0800

    提交user实体

---
+++ src/main/java/com/mis/common/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.mis.common.entity;
+
+/**
+ * Created by lbm on 2018/4/3.
+ */
+public class BaseEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/mis/login/entity/User.java
@@ -0,0 +1,128 @@
+package com.mis.login.entity;
+
+import com.mis.common.entity.BaseEntity;
+
+import java.util.Date;
+
+/**
+ * Created by lbm on 2018/4/3.
+ */
+public class User extends BaseEntity{
+    private String userName;
+    private String password;
+    private String realName;
+    private String nickName;
+    private String phone;
+    private String email;
+    private String address;
+    private Date birthday;
+    private int gender;
+    private Date createTime;
+    private Date updateTime;
+    private Date lastLoginTime;
+    private int loginFailedTimes;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public int getLoginFailedTimes() {
+        return loginFailedTimes;
+    }
+
+    public void setLoginFailedTimes(int loginFailedTimes) {
+        this.loginFailedTimes = loginFailedTimes;
+    }
+}
